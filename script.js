21ac0449451816a8ed1eb9b83bec06814acb2d3e
Mark Hermosa <alisonma@alisons-air.home>
2024-07-11 19:42:36 -0400

    added poem

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },  
+  {
+    author: "Mark Hermosa",
+    poem: "Roses are red \n Violets are blue \n pineapples on pizza, is good for you",
+    image: "bee.jpeg",
+  }, 
 ];
 
 let displayPoems = (array) => {
